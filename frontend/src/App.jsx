45dbd5606391bcad936873b1b88cebd09f9ae668
GeorgePauly95 <georgepauly1995@gmail.com>
2025-12-02 16:53:53 +0530

    remove message state and add conversations and conversation componenent

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,34 @@ import './App.css'
 
 function App() {
   return (
-    <div className="box">
+    <div className="box_outer">
+      <Conversations />
       <TextInput />
     </div>
   )
 }
 
+function Conversations() {
+  const [convs, setConvs] = useState([])
+  useEffect(() => {
+    fetch("/api/groups").then(response => response.json()).then(data => { setConvs(data) })
+  }, [])
+  return (
+    <div className='box_conv'>
+      {convs.map(conv => <Conversation conv={conv["name"]} />)}
+    </div >
+  )
+}
+function Conversation({ conv }) {
+  return (
+    <div className='conv'>
+      {conv}
+    </div>
+  )
+}
 
 function TextInput() {
-  const [message, setMessage] = useState("")
-  const [msgHst, setMsghst] = useState([])
+  const [msgHst, setMsghst] = useState([{ "messageContent": "" }])
 
   useEffect(() => {
     fetch("/api/messages").then(response => response.json())
@@ -21,55 +39,51 @@ function TextInput() {
       })
   }, [])
 
-  useEffect(() => {
-    fetch("/api/messages", {
-      method: "POST",
-      body: JSON.stringify({
-        "senderName": "George",
-        "messageContent": message["messageContent"],
-        "sentAt": "12-12-2024",
-        "readStatus": true,
-        "repliedTo": null,
-      })
-
-    })
-      .then(response => response.json())
-      .then(data => console.log(data))
-  },
-    [message])
-
-  function sentMessage(e) {
-    setMessage({
+  function sendMessage(e) {
+    setMsghst([...msgHst, {
       "senderName": "George",
       "messageContent": e.get("currmsg"),
       "sentAt": "12-12-2025",
       "readStatus": true,
       "repliedTo": null,
+    }])
+    fetch("/api/messages", {
+      method: "POST",
+      body: JSON.stringify({
+        "senderName": "George",
+        "messageContent": e.get("currmsg"),
+        "sentAt": "12-12-2024",
+        "readStatus": true,
+        "repliedTo": null,
+      })
     })
-    setMsghst([...msgHst, message])
+      .then(response => response.json())
+      .then(data => console.log(data))
+
   }
 
   return (
-    <div>
-      <form action={sentMessage}>
-        <input name="currmsg" type="text" className="currmsg" placeholder="enter message here" />
+    <div className='box_inner'>
+      <div className='box_messages'>
+        {<MessageHistory msgHst={msgHst} />}
+      </div>
+      <form action={sendMessage} className='form'>
+        <textarea name="currmsg" type="text" className="currmsg" placeholder="enter message here" />
         <button type="submit" className="sendbtn">Send</button>
       </form>
-      {<MessageHistory msgHst={msgHst} />}
-      {message && <MessageBlob message={message["messageContent"]} />}
     </div>
   )
 }
 
 function MessageHistory({ msgHst }) {
-  return (<div>
+  return (<div className='msgHst'>
     {msgHst.map(message => <MessageBlob message={message["messageContent"]} />)}
   </div>)
 }
 
 function MessageBlob({ message }) {
   return (
-    <div className='blob'>{message}</div>
+    <div className='msg'>{message}</div>
   )
 }
 
