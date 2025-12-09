18123b9358ef310cae0bd4a8cb234775fb55a277
GeorgePauly95 <georgepauly1995@gmail.com>
2025-12-09 12:32:28 +0530

    connect postgres to existing apis

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,12 @@
 from fastapi import FastAPI, Request
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+url = os.getenv("url")
+engine = create_engine(f"{url}", echo=True)
 app = FastAPI()
-
-messages = []
-
-groups = [
-    {
-        "id": 1,
-        "name": "Bit by Bit",
-        "createdAt": "12-12-2025",
-        "deletedAt": None,
-    },
-    {
-        "id": 2,
-        "name": "Aaqib",
-        "createdAt": "12-12-2025",
-        "deletedAt": None,
-    },
-]
 
 
 @app.get("/")
@@ -27,16 +16,43 @@ async def root():
 
 @app.get("/api/messages")
 async def show_messages():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM messages"))
+        messages = result.all()
+        conn.commit()
+    messages = [dict(message._mapping) for message in messages]
     return messages
 
 
 @app.post("/api/messages")
 async def send_message(request: Request):
     json_body = await request.json()
-    messages.append(json_body)
+    with engine.connect() as conn:
+        conn.execute(
+            text(
+                """INSERT INTO messages
+        (id, sender_id, group_id, content, sent_at, deleted_at, replied_to) VALUES 
+        (:id, :sender_id, :group_id, :content, :sent_at, :deleted_at, :replied_to)"""
+            ),
+            {
+                "id": json_body["id"],
+                "sender_id": json_body["sender_id"],
+                "group_id": json_body["group_id"],
+                "content": json_body["content"],
+                "sent_at": json_body["sent_at"],
+                "deleted_at": json_body["deleted_at"],
+                "replied_to": json_body["replied_to"],
+            },
+        )
+        conn.commit()
     return "message stored!"
 
 
 @app.get("/api/groups")
 async def show_groups():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM group_chats"))
+        groups = result.all()
+        conn.commit()
+    groups = [group._mapping for group in groups]
     return groups
