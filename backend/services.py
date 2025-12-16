d8b4bebe71a1f3001b44f62c7fb5c92a5e4ddbfe
GeorgePauly95 <georgepauly1995@gmail.com>
2025-12-16 19:39:53 +0530

    get user details doesn't need business logic isEmpty for message validation is required in the backend as well

--- backend/services.py
+++ backend/services.py
@@ -1,6 +1,5 @@
-def get_user_details(username, UserAccount, connection):
-    user_details = UserAccount.get_user_details(username, connection)
-    if user_details is None:
-        return False
-    user_id, username = user_details[2:4]
-    return {"user_id": user_id, "username": username}
+def is_empty(text):
+    cleaned_text = text.strip()
+    if cleaned_text == "":
+        return True
+    return False
