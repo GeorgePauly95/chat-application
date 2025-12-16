30edb708a6cd33dfc778b66953840aac1c66086b
GeorgePauly95 <georgepauly1995@gmail.com>
2025-12-16 19:41:24 +0530

    move creating connections to models

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI, Request, status
 from fastapi.responses import JSONResponse
 from engine import engine
-from models import Message, Group, UserAccount
-from services import get_user_details
+from models import Message, Group, UserAccount, User
 
 app = FastAPI()
 
@@ -14,32 +13,36 @@ async def root():
 
 @app.get("/api/messages/")
 async def show_messages(groupid: int):
-    connection = engine.connect()
-    return Message.showall_messages(groupid, connection)
+    return Message.showall_messages(groupid)
 
 
 @app.post("/api/messages")
 async def send_message(request: Request):
     request_body = await request.json()
-    connection = engine.connect()
-    return Message.add_message(request_body, connection)
+    response = Message.add_message(request_body)
+    if response is None:
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={})
+    return response
 
 
 @app.get("/api/groups")
 async def show_groups():
-    connection = engine.connect()
-    return Group.showall_groups(connection)
+    return Group.showall_groups()
 
 
 @app.post("/api/login")
 async def login(request: Request):
     request_body = await request.json()
-    connection = engine.connect()
     username = request_body["username"]
-    user_details = get_user_details(username, UserAccount, connection)
-    if not user_details:
+    user_details = UserAccount.get_user_details(username)
+    if user_details is None:
         return JSONResponse(
             status_code=status.HTTP_401_UNAUTHORIZED,
             content={"message": "Unauthorized"},
         )
     return user_details
+
+
+@app.get("/api/users")
+async def show_users():
+    return User.show_users()
