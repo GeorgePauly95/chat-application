85ee72a9509d17d8fb585a0a0f1eeb21d5264728
GeorgePauly95 <georgepauly1995@gmail.com>
2025-12-22 18:16:08 +0530

    add functionality to click on groups in side panel to load conversations

--- frontend/src/conversations.jsx
+++ frontend/src/conversations.jsx
@@ -1,21 +1,30 @@
 import { useState, useEffect } from 'react'
 import './conversations.css'
 
-function Conversations() {
+function Conversations({ user_id, setGroup_id }) {
   const [convs, setConvs] = useState([])
   useEffect(() => {
-    fetch("/api/groups").then(response => response.json()).then(data => { setConvs(data) })
-  }, [])
+
+    // need to change it to fetch groups for particular user id
+    fetch(`/api/groups/?user_id=${user_id}`)
+      .then(response => response.json())
+      .then(data => {
+        setConvs(data)
+      })
+  }, [user_id])
+
   return (
     <div className='box_conv'>
-      {convs.map(conv => <Conversation key={conv.id} conv={conv["name"]} />)}
+      {convs.map(conv => <Conversation key={conv.id} conv={conv} setGroup_id={setGroup_id} />)}
     </div >
   )
 }
-function Conversation({ conv }) {
+
+function Conversation({ conv, setGroup_id }) {
+  var id = conv["id"]
   return (
-    <div className='conv'>
-      {conv}
+    <div className='conv' onClick={() => { setGroup_id(id) }}>
+      {conv["name"]}
     </div>
   )
 }
