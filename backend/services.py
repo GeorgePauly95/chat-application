9a0879878050cfe5d541b90d78b83030e3a4b646
GeorgePauly95 <georgepauly1995@gmail.com>
2026-01-04 11:49:27 +0530

    web socket connections manager

--- backend/services.py
+++ backend/services.py
@@ -1,3 +1,25 @@
+from fastapi import WebSocket
+
+
+class ConnectionManager(dict):
+    def __init__(self):
+        self.active_connections: dict[int, WebSocket] = {}
+
+    async def create_connection(self, websocket: WebSocket, user_id: int):
+        await websocket.accept()
+        self.active_connections.update({user_id: websocket})
+
+    def get_connection(self, user_id: int):
+        connection = self.active_connections.get(user_id)
+        return connection
+
+    async def send_message(self, message, websocket: WebSocket):
+        await websocket.send_json(message)
+
+    def remove_connection(self, user_id: int):
+        del self.active_connections[user_id]
+
+
 def is_empty(text):
     cleaned_text = text.strip()
     if cleaned_text == "":
